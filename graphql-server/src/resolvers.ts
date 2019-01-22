80ccbed6c2eb2e309c6ed2525b3191585ab20ec6
Ricky Singh <singh.sangh@gmail.com>
2019-01-22 15:55:01 -0800

    added resolver map

--- graphql-server/src/resolvers.ts
+++ graphql-server/src/resolvers.ts
@@ -1,8 +1,12 @@
-import {IResolvers} from "graphql-middleware/dist/types";
+//import {IResolvers} from "graphql-middleware/dist/types";
+import {ResolverMap} from "./types/graphql-utils";
 
 
-export const resolvers: IResolvers = {
+export const resolvers: ResolverMap = {
     Query: {
-        hello: (_:any, { name }: GQL.IHelloOnQueryArguments) => `Hola ${name || 'Mundo'}`,
+        hello: (_:any, { name }: GQL.IHelloOnQueryArguments) => `Hola ${name || "Mundo"}`,
     },
+    Mutation: {
+        register: (_, { email, password }: GQL.IRegisterOnMutationArguments) => {}
+    }
 };
---
+++ graphql-server/src/types/graphql-utils.ts
@@ -0,0 +1,7 @@
+export interface ResolverMap {
+    [key: string]: {
+        [key: string]: (parent: any, args: any, context: {}, info: any) => any;
+
+
+    }
+}
