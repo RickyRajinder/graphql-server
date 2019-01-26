f43b9338bb20c06d9bda248764fb0288f1f82f3a
Ricky Singh <singh.sangh@gmail.com>
2019-01-25 21:44:55 -0800

    set up test db

--- graphql-server/src/index.ts
+++ graphql-server/src/index.ts
@@ -2,14 +2,18 @@ import "reflect-metadata";
 import { importSchema } from 'graphql-import'
 import { GraphQLServer } from 'graphql-yoga';
 import { resolvers } from "./resolvers";
-import {createConnection} from "typeorm";
 import * as path from 'path';
-
-const typeDefs = importSchema(path.join(__dirname, "./schema.graphql"));
-
+import {createTypeORMConn} from "./utils/createTypeORMConn";
 
 
-const server = new GraphQLServer({ typeDefs, resolvers });
-createConnection().then(() => {
-    server.start(() => console.log('Server is running on localhost:4000'));
-});
+export const startServer = async () => {
+    const typeDefs = importSchema(path.join(__dirname, "./schema.graphql"));
+
+    const server = new GraphQLServer({ typeDefs, resolvers });
+    await createTypeORMConn();
+    await server.start();
+    console.log('Server is running on localhost:4000');
+};
+
+startServer();
+
--- graphql-server/src/tests/register.test.ts
+++ graphql-server/src/tests/register.test.ts
@@ -1,7 +1,28 @@
-function sum(a, b){
-    return a + b;
-}
+import { request } from 'graphql-request';
+import {host} from "../constants";
+import {User} from "../entity/User";
+import {createTypeORMConn} from "../utils/createTypeORMConn";
 
-test("adds 1 + 2 to equal 3", () => {
-    expect(sum(1,2)).toBe(3);
+beforeAll( async () => {
+    await createTypeORMConn();
+})
+
+const email = "tob@bob.com";
+const password = "fdgdfhdsf";
+
+const mutation = `
+mutation {
+    register(email: "${email}", password: "${password}")
+}
+`;
+
+test("Register user", async (done) => {
+    const response = await request(host, mutation)
+    expect(response).toEqual({ register: true })
+    const users = await User.find({ where: { email } });
+    expect(users).toHaveLength(1);
+    const user = users[0];
+    expect(user.email).toEqual(email);
+    expect(user.password).not.toEqual(password);
+    done();
 });
